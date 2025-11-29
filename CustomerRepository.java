76bbf012e80f8e73396197b91398dc411139abe4
Ivan <i.a.konuhov@gmail.com>
2025-11-29 15:58:30 +0700

    Unexpected fixes

--- CustomerRepository.java
+++ CustomerRepository.java
@@ -1,7 +1,13 @@
 import java.util.List;
+import java.util.Map;
 public class CustomerRepository extends Repository<Customer>
 {
- // Дополнительные методы:
- public List<Customer> findCustomersByEmail(String email)
- public List<Customer> findCustomersByName(String name)
+    // Дополнительные методы:
+    public CustomerRepository(Map<String, Customer> items, String repositoryName)
+    {
+        super(items, repositoryName);
+    }
+
+    public List<Customer> findCustomersByEmail(String email) { return null; }
+    public List<Customer> findCustomersByName(String name) { return null; }
 }
--- OrderRepository.java
+++ OrderRepository.java
@@ -1,7 +1,15 @@
 import java.util.List;
+import java.util.Map;
 
-public class OrderRepository extends Repository<Order> {
- // Дополнительные методы:
- public List<Order> getOrdersByCustomer(String customerId)
- public List<Order> getOrdersByStatus(String status)
+public class OrderRepository extends Repository<Order>
+{
+    // Дополнительные методы:
+    public OrderRepository(Map<String, Order> items, String repositoryName)
+    {
+        super(items, repositoryName);
+        //TODO Auto-generated constructor stub
+    }
+
+    public List<Order> getOrdersByCustomer(String customerId) {return null;}
+    public List<Order> getOrdersByStatus(String status) {return null;}
 }
